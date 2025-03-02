86b8abdd725f84e29398c92e5b15ef27382152a9
Myronarty <ahamalianmyron@gmail.com>
2025-03-02 12:26:35 +0200

    1 test

--- lab/Source.cpp
+++ lab/Source.cpp
@@ -18,3 +18,49 @@ int Cayley_table(int a)
     }
     return a;
 }
+
+vector<int> v(int a)
+{
+	vector<int> h;
+	double b = a;
+	int m = log2(a);
+	for (int i = 0; i <= m; i++)
+	{
+		if (b - pow(2, m - i) >= 0)
+		{
+			h.push_back(1);
+			b = b - pow(2, m - i);
+		}
+		else
+		{
+			h.push_back(0);
+		}
+	}
+	for (int i = 0; i <= h.size() - 1; i++)
+	{
+		//cout << h[i];
+	}
+	//cout <<  endl;
+	return h;
+}
+
+
+int Gorn(int a, int b, int n)
+{
+	vector<int> h = v(b);
+	int k = 1;
+	int t = h.size() - 1;
+	for (int i = 0; i < t; i++)
+	{
+		if (h[i] == 1) 
+		{
+			k = k * a;
+		}		
+		k = k*k;
+		k = k % n;
+	}
+	k = k * pow(a, h[t]);
+	k = k % n;
+	//cout << k;
+	return k;
+}
